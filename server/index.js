c1c79af7c373a190cd37a3a1ec48128f41b6a73c
Ahlyab <ahlyabasad@gmail.com>
2025-03-13 21:58:38 +0500

    fixes

--- server/index.js
+++ server/index.js
@@ -11,36 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://inventory-manager-ten-eta.vercel.app",
-    "http://localhost:5173",
-    "https://inventory-manager-5djf.vercel.app",
-  ];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With"
-  );
-  res.setHeader("Access-Control-Max-Age", "86400");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  next();
-});
-app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // Routes
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Inventory and Revenue Management API is running");
 });
