245310b6623d5ad862e8a9e1747ab910d4e7e7fe
Ahlyab <ahlyabasad@gmail.com>
2025-03-13 22:22:15 +0500

    added title

--- server/index.js
+++ server/index.js
@@ -14,9 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*", // Allow all origins
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    origin: "*",
   })
 );
 // Routes
